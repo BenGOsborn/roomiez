dd27532c1e6dc7de0de452876860e99c2b1f5343
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-07-08 11:05:35 +1000

    Added lambda api gateway integration (no CORS)

--- aws/main.tf
+++ aws/main.tf
@@ -11,11 +11,24 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+# Secrets manager
+
+resource "aws_secretsmanager_secret" "secrets" {
+  name = "secrets"
+}
+
 # API gateway
 
 resource "aws_api_gateway_rest_api" "rest_api" {
   name        = "roomiez-api"
 }
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = "prod" 
+}
+
+# API resources and methods
 
 resource "aws_api_gateway_resource" "rentals_resource" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
@@ -23,27 +36,51 @@ resource "aws_api_gateway_resource" "rentals_resource" {
   path_part   = "rentals"
 }
 
+resource "aws_api_gateway_method" "rentals_get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.rentals_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.rentals_resource.id
+  http_method             = aws_api_gateway_method.rentals_get_method.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri  = aws_lambda_function.retrieve_rentals_lambda.invoke_arn
+}
+
 # Lambda
 
 resource "aws_lambda_function" "retrieve_rentals_lambda" {
   function_name = "retrieve-rentals"
-  role          = aws_iam_role.retrieve_rentals_lambda_role.arn
+  role          = aws_iam_role.lambda_role.arn
   handler       = "main"
   runtime = "go1.x"
   filename      = "retrieve_rentals.zip"
+  source_code_hash = filebase64sha256("retrieve_rentals.zip")
 
   environment {
     variables = {
-      KEY1 = "VALUE1"
-      KEY2 = "VALUE2"
+      SECRETS = aws_secretsmanager_secret.secrets.arn
     }
   }
 }
 
+resource "aws_lambda_permission" "apigw_retrieve_rentals_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.retrieve_rentals_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/${aws_api_gateway_method.rentals_get_method.http_method}${aws_api_gateway_resource.rentals_resource.path}"
+}
+
 # Roles
 
-resource "aws_iam_role" "retrieve_rentals_lambda_role" {
-  name = "retrieve-rentals-lambda-role"  # Replace with your desired IAM role name
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
 
   assume_role_policy = <<EOF
 {
@@ -59,4 +96,21 @@ resource "aws_iam_role" "retrieve_rentals_lambda_role" {
   ]
 }
 EOF
+}
+
+data "aws_iam_policy_document" "secrets_manager_policy" {
+  statement {
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = [aws_secretsmanager_secret.secrets.arn]
+  }
+}
+
+resource "aws_iam_policy" "secrets_manager_policy" {
+  name        = "secrets-manager-policy"
+  policy      = data.aws_iam_policy_document.secrets_manager_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_secrets_manager_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.secrets_manager_policy.arn
 }
--- aws/retrieve_rentals/main.go
+++ aws/retrieve_rentals/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
+func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) {
+	responseBody := "Hello, world!"
+	headers := map[string]string{
+		"Content-Type": "text/plain",
+	}
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+	response := events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    headers,
+		Body:       responseBody,
+	}
+
+	return response, nil
 }
 
 func main() {
