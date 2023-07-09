c528e7e155b53867bcd625fabb72b57b84680cee
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-07-10 08:19:05 +1000

    Updated search

--- aws/src/utils/search_rentals.go
+++ aws/src/utils/search_rentals.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -21,7 +23,18 @@ type SearchParams struct {
 
 // Find a list of rentals that match the search params
 func SearchRentals(db *gorm.DB, searchParams *SearchParams, perPage uint) (*[]Rental, error) {
-	query := db.Model(&Rental{})
+	query := db.Table("rentals")
+
+	query = query.Select("rentals.url", "rentals.coordinates", "rentals.price", "rentals.bond",
+		"rental_types.type", "genders.preference AS gender_preference",
+		"ages.preference AS age_preference", "durations.preference AS duration_preference", "tenants.preference AS tenant_preference",
+	)
+
+	query = query.Joins("JOIN rental_types ON rentals.rental_type_id = rental_types.id").
+		Joins("JOIN genders ON rentals.gender_id = genders.id").
+		Joins("JOIN ages ON rentals.age_id = ages.id").
+		Joins("JOIN durations ON rentals.duration_id = durations.id").
+		Joins("JOIN tenants ON rentals.tenant_id = tenants.id")
 
 	if searchParams.Price != nil {
 		query = query.Where("price = ?", *searchParams.Price)
@@ -32,23 +45,23 @@ func SearchRentals(db *gorm.DB, searchParams *SearchParams, perPage uint) (*[]Re
 	}
 
 	if searchParams.RentalType != nil {
-		query = query.Joins("JOIN rental_types ON rentals.rental_type_id = rental_types.id").Where("rental_types.type = ?", *searchParams.RentalType)
+		query = query.Where("rental_types.type = ?", *searchParams.RentalType)
 	}
 
 	if searchParams.Gender != nil {
-		query = query.Joins("JOIN genders ON rentals.gender_id = genders.id").Where("genders.preference = ?", *searchParams.Gender)
+		query = query.Where("genders.preference = ?", *searchParams.Gender)
 	}
 
 	if searchParams.Age != nil {
-		query = query.Joins("JOIN ages ON rentals.age_id = ages.id").Where("ages.preference = ?", *searchParams.Age)
+		query = query.Where("ages.preference = ?", *searchParams.Age)
 	}
 
 	if searchParams.Duration != nil {
-		query = query.Joins("JOIN durations ON rentals.duration_id = durations.id").Where("durations.preference = ?", *searchParams.Duration)
+		query = query.Where("durations.preference = ?", *searchParams.Duration)
 	}
 
 	if searchParams.Tenant != nil {
-		query = query.Joins("JOIN tenants ON rentals.tenant_id = tenants.id").Where("tenants.preference = ?", *searchParams.Tenant)
+		query = query.Where("tenants.preference = ?", *searchParams.Tenant)
 	}
 
 	if searchParams.Latitude != nil && searchParams.Longitude != nil && searchParams.Radius != nil {
@@ -58,9 +71,14 @@ func SearchRentals(db *gorm.DB, searchParams *SearchParams, perPage uint) (*[]Re
 	query = query.Offset((int(searchParams.Page) - 1) * int(perPage)).Limit(int(perPage))
 
 	rentals := &[]Rental{}
-	if err := query.Find(rentals).Error; err != nil {
+
+	result := make(map[string]interface{})
+
+	if err := query.Find(result).Error; err != nil {
 		return nil, err
 	}
+
+	fmt.Println(result)
 
 	return rentals, nil
 }
