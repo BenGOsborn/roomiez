902bae8ca5e0fb28a7b5ce3df70030822eead863
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-07-10 22:37:26 +1000

    Working get fields

--- aws/main.tf
+++ aws/main.tf
@@ -75,6 +75,28 @@ resource "aws_api_gateway_integration" "rentals_post_lambda_integration" {
   uri                     = aws_lambda_function.process_rental_lambda.invoke_arn
 }
 
+resource "aws_api_gateway_resource" "rentals_fields_resource" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_resource.rentals_resource.id
+  path_part   = "fields"
+}
+
+resource "aws_api_gateway_method" "rentals_fields_get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.rentals_fields_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "rentals_fields_get_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.rentals_fields_resource.id
+  http_method             = aws_api_gateway_method.rentals_fields_get_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.get_fields_lambda.invoke_arn
+}
+
 # Lambda
 
 resource "aws_lambda_function" "retrieve_rentals_lambda" {
@@ -111,6 +133,23 @@ resource "aws_lambda_function" "process_rental_lambda" {
   }
 }
 
+resource "aws_lambda_function" "get_fields_lambda" {
+  function_name    = "get-fields"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "main"
+  runtime          = "go1.x"
+  timeout          = "30"
+  filename         = "get_fields.zip"
+  source_code_hash = filebase64sha256("get_fields.zip")
+
+  environment {
+    variables = {
+      SECRETS_ARN = aws_secretsmanager_secret.secrets.arn
+      ENV         = "production"
+    }
+  }
+}
+
 resource "aws_lambda_permission" "apigw_retrieve_rentals_lambda_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
@@ -125,6 +164,14 @@ resource "aws_lambda_permission" "apigw_process_rental_lambda_permission" {
   function_name = aws_lambda_function.process_rental_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/${aws_api_gateway_method.rentals_post_method.http_method}${aws_api_gateway_resource.rentals_resource.path}"
+}
+
+resource "aws_lambda_permission" "apigw_get_fields_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_fields_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/${aws_api_gateway_method.rentals_fields_get_method.http_method}${aws_api_gateway_resource.rentals_fields_resource.path}"
 }
 
 # Roles
