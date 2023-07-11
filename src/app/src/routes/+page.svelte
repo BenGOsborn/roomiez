095fa92c4713a7fb68ea1662477a41433db23a93
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-07-11 10:36:19 +1000

    Initialized svelte

---
+++ src/app/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let x = 0;
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>{x}</p>
+<button on:click={() => x = x + 1}>Increment</button>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
