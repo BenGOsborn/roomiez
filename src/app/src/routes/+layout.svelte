1a5c6398cf092470069695c61b16e46ada32909f
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-07-13 13:40:29 +1000

    Added google analytics

--- src/app/src/routes/+layout.svelte
+++ src/app/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { PUBLIC_MAPS_KEY } from "$env/static/public";
+	import { PUBLIC_GA_ID, PUBLIC_MAPS_KEY } from "$env/static/public";
 	import "../app.css";
 	import Nav from "../components/Nav.svelte";
 	import { mapReady } from "../stores";
@@ -8,11 +8,25 @@
 	window.initMap = () => {
 		mapReady.set(true);
 	};
+
+	// @ts-ignore
+	window.dataLayer = window.dataLayer || [];
+
+	function gtag() {
+		// @ts-ignore
+		dataLayer.push(arguments);
+	}
+
+	// @ts-ignore
+	gtag("js", new Date());
+	// @ts-ignore
+	gtag("config", PUBLIC_GA_ID);
 </script>
 
 <svelte:head>
 	<script defer async src={`https://maps.googleapis.com/maps/api/js?key=${PUBLIC_MAPS_KEY}&libraries=places&callback=initMap`}>
 	</script>
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GA_ID}`}></script>
 
 	<title>Roomiesz - Find A Shared Rental</title>
 	<meta
