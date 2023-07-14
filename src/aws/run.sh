8bdbfe0edfca1f051d73ad25a431e32ffaeb224e
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-07-14 22:19:14 +1000

    Initialized web scraper

--- src/aws/run.sh
+++ src/aws/run.sh
@@ -4,6 +4,7 @@ go build retrieve_rentals/main.go && zip ../retrieve_rentals.zip main && rm main
 go build process_rental/main.go && zip ../process_rental.zip main && rm main
 go build get_fields/main.go && zip ../get_fields.zip main && rm main
 go build subscribe/main.go && zip ../subscribe.zip main && rm main
+cd scrape && npm install --production && zip ../scrape.zip * -r && cd ..
 
 cd ..
 
---
+++ src/aws/src/scrape/index.js
@@ -0,0 +1,54 @@
+exports.handler = async (event, context) => {};
+
+(async () => {
+    if (process.env.ENV !== "production") {
+        require("dotenv").config({ path: "../../.env" });
+    }
+
+    const AWS = require("aws-sdk");
+    const secretsManager = new AWS.SecretsManager({ region: "ap-southeast-2" });
+    const puppeteer = require("puppeteer-core");
+
+    // Load env
+    const secretsArn = process.env.SECRETS_ARN;
+
+    const secretsString = (await secretsManager.getSecretValue({ SecretId: secretsArn }).promise()).SecretString;
+    if (!secretsString) throw Error("could not retrieve secrets");
+
+    const secrets = JSON.parse(secretsString);
+
+    // Initialize browser
+    let browser = await puppeteer.connect({ browserWSEndpoint: secrets["BROWSER_ENDPOINT"] });
+
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(2 * 60 * 1000);
+
+    await page.goto("https://www.facebook.com/groups/2280085492006745");
+    await page.waitForSelector('[role="feed"]');
+
+    // **** If it works we will loop through this by scrolling down N times
+    const html = await page.evaluate(async () => {
+        const elements = Array.from(document.querySelector('[role="feed"]').children).slice(1);
+        const out = [];
+
+        for (const elem of elements) {
+            const msg = elem.querySelector('[data-ad-comet-preview="message"]');
+            const more = elem.querySelector('[role="button"]');
+
+            if (msg) {
+                if (more) {
+                    more.click();
+                    await new Promise((res) => setTimeout(res, 100));
+                }
+
+                out.push(msg.textContent);
+            }
+        }
+
+        return out;
+    });
+
+    console.log(html);
+
+    await browser?.close();
+})();
