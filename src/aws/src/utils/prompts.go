290ca44d89c834ec54b59051ce63c9903dfabf78
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-07-18 09:15:57 +1000

    Update

--- src/aws/src/utils/prompts.go
+++ src/aws/src/utils/prompts.go
@@ -62,7 +62,7 @@ func NewPostExtraction(llm *openai.Chat) *chains.LLMChain {
 		age: "Young" | "Middle Aged" | "Old" | null;
 		duration: "Short Term" | "Long Term" | null;
 		tenant: "Singles" | "Couples" | null;
-		features: ("Garage" | "WiFi" | "Bills Included" | "Furnished" | "Pets Allowed" | "Garage" | "Mattress" | "Pool" | "Gym")[];
+		features: ("Garage" | "WiFi" | "Bills Included" | "Furnished" | "Pets Allowed" | "Mattress" | "Pool" | "Gym")[];
 	}
 
 	Some examples have been provided below to give you context.
