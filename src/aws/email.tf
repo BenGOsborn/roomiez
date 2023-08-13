6ba8d85c31e9bec4b3a41339688e3862bb869b00
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-08-13 11:59:24 +1000

    Fixed email scheduler

--- src/aws/email.tf
+++ src/aws/email.tf
@@ -10,6 +10,14 @@ resource "aws_cloudwatch_event_target" "schedule" {
   arn  = aws_lambda_function.schedule_email_lambda.arn
 }
 
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.schedule_email_lambda.arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.weekly_email.arn
+}
+
 # Queue
 
 resource "aws_sqs_queue" "email_queue" {
